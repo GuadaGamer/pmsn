9bf5bf8a07815ff9d687c244523e77615c8728a0
GuadaGamer <guadalupesv2000@outlook.com>
2023-03-05 10:05:21 -0600

    SharedPreference

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,25 @@ import 'package:provider/provider.dart';
 import 'package:psmnn/provider/theme_provider.dart';
 import 'package:psmnn/routes.dart';
 import 'package:psmnn/screens/home_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences sharedPreferences= await SharedPreferences.getInstance();
+  final idTema=sharedPreferences.getInt('id_tema')??0;
+  runApp(MyApp( id_tema  :idTema));
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final int id_tema;
+  const MyApp({super.key, required this.id_tema});
+  
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => ThemeProvider(context)),
+        ChangeNotifierProvider(create: (_) => ThemeProvider(id_tema, context)),
       ],
       child: const PMSNApp(),
     );
