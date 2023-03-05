fcf4faf6baeb7dd2dba2231ca9f217527ce97189
GuadaGamer <guadalupesv2000@outlook.com>
2023-03-05 01:27:39 -0600

    Responsive onboarding

--- lib/screens/cards/card_tec.dart
+++ lib/screens/cards/card_tec.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:psmnn/responsive.dart';
 
 class CardTecData {
   final String title;
@@ -27,45 +28,99 @@ class CardTec extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        if(data.backgraund != null) data.backgraund!,
-        Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 20,
-            vertical: 40,
-          ),
-          child: Column(
-            children: [
-              const Spacer(flex: 3,),
-              Flexible(
-                flex: 20,
-                child: Image(image: data.image)),
-              const Spacer(flex: 1,),
-              Text(
-                data.title.toUpperCase(),
-                style: TextStyle(
-                  color: data.titleColor,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 1,
+    return Responsive(
+      mobile: Stack(
+        children: [
+          if(data.backgraund != null) data.backgraund!,
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 40,
+            ),
+            child: Column(
+              children: [
+                const Spacer(flex: 3,),
+                Flexible(
+                  flex: 20,
+                  child: Image(image: data.image)),
+                const Spacer(flex: 1,),
+                Text(
+                  data.title.toUpperCase(),
+                  style: TextStyle(
+                    color: data.titleColor,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1,
+                  ),
                 ),
+                const Spacer(flex: 1,),
+                Text(
+                  data.subtitle,
+                  style: TextStyle(
+                    color: data.subtitleColor,
+                    fontSize: 15,
+                    letterSpacing: 1,
+                  ),
+                  textAlign: TextAlign.center,
+                  maxLines: 4,
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+      desktop: Stack(
+        children: [
+          if (data.backgraund != null) data.backgraund!,
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 260),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Flexible(
+                    child: Image(image: data.image),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        data.title.toUpperCase(),
+                        style: TextStyle(
+                            color: data.titleColor,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 1),
+                            textAlign: TextAlign.center,
+                        maxLines: 2,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ), 
+                     
+                      Container(
+                        height: 200,
+                        width: 300,
+                        child: Text(
+                          data.subtitle,
+                          style: TextStyle(
+                            color: data.subtitleColor,
+                            fontSize: 16,
+                          ),
+                          textAlign: TextAlign.center,
+                          textWidthBasis: TextWidthBasis.longestLine,
+                          maxLines: 9,
+                        ),
+                      ),
+                      
+                    ],
+                  ),
+                ],
               ),
-              const Spacer(flex: 1,),
-              Text(
-                data.subtitle,
-                style: TextStyle(
-                  color: data.subtitleColor,
-                  fontSize: 15,
-                  letterSpacing: 1,
-                ),
-                textAlign: TextAlign.center,
-                maxLines: 4,
-              )
-            ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,26 +11,34 @@ class Home extends StatelessWidget {
         title: "TecNM en Celaya",
         subtitle: "¡Orgullosamente, LINCES!",
         image: const AssetImage('assets/itce.png'),
-        backgraundColor: const Color.fromARGB(171, 10, 56, 1),
+        backgraundColor: const Color.fromARGB(255, 43, 120, 129),
         titleColor: Colors.pink,
         subtitleColor: Colors.white,
-        backgraund: Lottie.asset('assets/animation/bg-7.json')),
+        backgraund: Lottie.asset('assets/animation/bg-5.json')),
     CardTecData(
         title: "Misión",
         subtitle: "Contribuir a la transformación de la sociedad a través de la formación de ciudadanas y ciudadanos libres y responsables",
         image: const AssetImage('assets/mision.png'),
-        backgraundColor: const Color.fromARGB(146, 63, 192, 37),
+        backgraundColor: const Color.fromARGB(255, 17, 148, 139),
         titleColor: Colors.green,
         subtitleColor: Colors.black,
         backgraund: Lottie.asset('assets/animation/bg-2.json')),
     CardTecData(
         title: "Visión",
-        subtitle: "Ser una institución de educación superior tecnológica reconocida a nivel internacional por el destacado desempeño de sus egresadas y egresados",
+        subtitle: "Ser una institución de educación superior tecnológica reconocida a nivel internacional por el destacado dersempeño de sus egresadas y egresados",
         image: const AssetImage('assets/vision.png'),
-        backgraundColor: const Color.fromARGB(120, 57, 27, 141),
+        backgraundColor: const Color.fromARGB(255, 102, 74, 114),
         titleColor: Colors.white,
-        subtitleColor: Colors.blueGrey,
-        backgraund: Lottie.asset('assets/animation/bg-11.json'))
+        subtitleColor: Colors.blue,
+        backgraund: Lottie.asset('assets/animation/bg-11.json')),
+    CardTecData(
+      title: "Ingresa a la red social",
+      subtitle: "Presiona el botón para inciar sesión",
+      image: const AssetImage('assets/lince.png'),
+      backgraundColor: const Color.fromARGB(255, 137, 19, 60),
+      titleColor: Colors.pink,
+      subtitleColor: Colors.white,
+      backgraund: Lottie.asset("assets/animation/bg-3.json")),
   ];
 
   @override
