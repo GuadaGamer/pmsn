25cd4a9487f4b7b1a9769f016f53db8126c19efb
GuadaGamer <guadalupesv2000@outlook.com>
2023-04-04 00:23:44 -0600

    token update

--- lib/network/api_popular.dart
+++ lib/network/api_popular.dart
@@ -18,20 +18,20 @@ class ApiPopular {
     return null;
   }
 
-  Future<String> getVideo(int id_popular)async{
+  Future<String> getVideo(int id_popular) async {
     Uri linkES = Uri.parse(
-      'https://api.themoviedb.org/3/movie/$id_popular/videos?api_key=d7236b730825fb7b3c7e23e7d91e473c&language=es-MX');
+        'https://api.themoviedb.org/3/movie/$id_popular/videos?api_key=89dbf5270b59edd2f28730765b489b9e&language=es-MX');
     Uri linkEN = Uri.parse(
-      'https://api.themoviedb.org/3/movie/$id_popular/videos?api_key=d7236b730825fb7b3c7e23e7d91e473c');
+        'https://api.themoviedb.org/3/movie/$id_popular/videos?api_key=89dbf5270b59edd2f28730765b489b9e');
     var result = await http.get(linkES);
     var listJSON = jsonDecode(result.body)['results'] as List;
-    if (listJSON.isEmpty){
+    if (listJSON.isEmpty) {
       result = await http.get(linkEN);
       listJSON = jsonDecode(result.body)['results'] as List;
     }
-    if(result.statusCode==200){
+    if (result.statusCode == 200) {
       for (var element in listJSON) {
-        if(element['type']=='Trailer'){
+        if (element['type'] == 'Trailer') {
           return element['key'];
         }
       }
@@ -39,11 +39,12 @@ class ApiPopular {
     return '';
   }
 
-  Future<List<ActorModel>?> getAllActors(PopularModel popularModel) async{
-    Uri link= Uri.parse('https://api.themoviedb.org/3/movie/${popularModel.id}/credits?api_key=d7236b730825fb7b3c7e23e7d91e473c');
+  Future<List<ActorModel>?> getAllActors(PopularModel popularModel) async {
+    Uri link = Uri.parse(
+        'https://api.themoviedb.org/3/movie/${popularModel.id}/credits?api_key=89dbf5270b59edd2f28730765b489b9e');
     var result = await http.get(link);
     var listJSON = jsonDecode(result.body)['cast'] as List;
-    if(result.statusCode==200){
+    if (result.statusCode == 200) {
       return listJSON.map((actor) => ActorModel.fromMap(actor)).toList();
     }
     return null;
